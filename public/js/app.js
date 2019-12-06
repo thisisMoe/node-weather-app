701b78036ef966ab9ce55a6e5d71ea2c60bd4694
thisisMoe <bounoua.smm@gmail.com>
2019-12-06 15:18:23 +0100

    Setup app for Heroku

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,3 @@
-console.log('Client side JavaScript has loaded!');
-
 const weatherForm = document.querySelector('form');
 const search = document.querySelector('input');
 const messageOne = document.querySelector('#message-1');
@@ -11,7 +9,7 @@ weatherForm.addEventListener('submit', (e) => {
 	messageOne.textContent = 'Loading...';
 	messageTwo.textContent = '';
 
-	fetch('http://localhost:3000/weather?address=' + location).then((response) => {
+	fetch('/weather?address=' + location).then((response) => {
 		response.json().then((data) => {
 			if (data.error) {
 				messageOne.textContent = data.error;
